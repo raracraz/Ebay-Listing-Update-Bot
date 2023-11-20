490b31d26ae01ff2d2d0e2d2d15f2b6da891f728
raynac <ryan.aaixe@gmail.com>
2023-11-21 01:06:12 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Ebay Listing Update Bot
 This is a bot that monitors eBay seller listings and sends notifications to your Telegram account when there are updates.
+
+## Features
+- [x] Monitor eBay seller listings
+- [x] Send notifications to Telegram account/group
+
+## Usage
+1. Clone this repository
+2. Install dependencies
+```py -m pip install -r requirements.txt```
+3. Create a Telegram bot and get the bot token
+4. Create a Telegram group and add the bot to the group
+5. Get the group chat id
+6. Register a developer account on eBay and get the app id
+7. Create a Config.ini file in the root directory of the repository and fill in the details
+```
+[TelegramBotConfig]
+token = YOUR_BOT_TOKEN
+my_user_id = YOUR_USER_ID
+dest_channel_id = YOUR_GROUP_CHAT_ID
+
+[EbayConfig]
+appid = YOUR_EBAY_APP_ID
+
+[EbayFilters]
+seller_name = SELLER_NAME (in URL of seller's page)
+```
--- updaterBot.py
+++ updaterBot.py
@@ -1,24 +1,78 @@
 import configparser
-import re
+from datetime import datetime, timedelta
 import time
-import os
+import pytz 
+import json
 import logging
+from ebaysdk.finding import Connection as Finding
+from ebaysdk.exception import ConnectionError
 from telethon import TelegramClient, events, sync
+import telegram
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import CallbackContext, MessageHandler, Filters, Updater
 
-logging.basicConfig(level=logging.DEBUG)
+
+logging.basicConfig(level=logging.INFO)
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-token = config.get('BotConfig', 'token')
+appid = config.get('EbayConfig', 'appid')
+token = config.get('TelegramBotConfig', 'token')
+my_user_id = int(config.get('TelegramBotConfig', 'my_user_id'))
+dest_channel_id = config.get('TelegramBotConfig', 'dest_channel_id')
 
-def run_bot():
+def get_updated_listings(seller_name, last_checked):
     try:
-        updater = Updater(token, use_context=True)
-        
-    except Exception as e:
-        print(f"Unexpected error: {e}. Retrying in 10 seconds...")
-        time.sleep(10)
-        run_bot()
+        appid = config.get('EbayConfig', 'appid')
+        api = Finding(appid=appid, config_file=None)
+        response = api.execute('findItemsAdvanced', {
+            'itemFilter': [
+                {'name': 'Seller', 'value': str(seller_name)},
+                {'name': 'StartTimeFrom', 'value': last_checked.strftime('%Y-%m-%dT%H:%M:%S')}
+            ]
+        })
+        with open('output.json', 'w') as f:
+            json.dump(response.dict(), f, indent=4, sort_keys=True)
+        return response.dict()
+    except ConnectionError as e:
+        print(e)
+        print(e.response.dict())
+
+def main():
+    seller_name = config.get('EbayFilters', 'seller_name')
+    # Initialize last_checked to one day before the current time
+    gmt = pytz.timezone('GMT')
+    last_checked = datetime.now(gmt) - timedelta(days=1)
+
+    updater = Updater(token, use_context=True)
+
+    while True:
+        updated_listings = get_updated_listings(seller_name, last_checked)
+
+        # Update last_checked to the current time before checking for new listings
+        current_time = datetime.now(gmt)
+
+        if updated_listings.get('searchResult', {}).get('_count', '0') != '0':
+            items = updated_listings['searchResult']['item']
+            print(f"Found {len(items)} new listings.")
+
+            for item in items:
+                message = f"New item: {item['title']} at {item['viewItemURL']}"
+                try:
+                    updater.bot.send_message(chat_id=dest_channel_id, text=message)
+                    time.sleep(1)  # Short pause to prevent hitting rate limit
+                except telegram.error.RetryAfter as e:
+                    print(f"Rate limit exceeded. Waiting for {e.retry_after} seconds.")
+                    time.sleep(e.retry_after)
+                    updater.bot.send_message(chat_id=dest_channel_id, text=message)
+        else:
+            print("No new listings found.")
+
+        # Update last_checked to the time before the listings check
+        last_checked = current_time
+        time.sleep(60)  # Wait for 1 minutes before checking again
+
+if __name__ == '__main__':
+    main()
+
